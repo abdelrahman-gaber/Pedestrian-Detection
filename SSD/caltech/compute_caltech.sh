0b98317b7015b912bde272d5b59f7310593813fe
abdelrahman-gaber <eng.abdelrahman.gaber@gmail.com>
2017-08-22 13:49:11 +0200

    add evaluation matlab codes from caltech

--- SSD/caltech/compute_caltech.sh
+++ SSD/caltech/compute_caltech.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-module load anaconda
-module load gcc/5.3.0 
-module load opencv2/gcc5
+#module load anaconda
+#module load gcc/5.3.0 
+#module load opencv2/gcc5
 module load cuda/8.0 
 module load cudnn/5.1-cuda-8.0
-module load caffe_priv/ssd
+#module load caffe_priv/ssd
 
 dataset_path=/data/stars/user/aabubakr/pd_datasets/datasets/caltech/annot-images/images/test
-save_path=/data/stars/user/aabubakr/pd_datasets/outputs/SSD-OUT/caltech/test-ft-cal+inria
+save_path=/data/stars/user/aabubakr/pd_datasets/outputs/SSD-OUT/caltech/test-ft-inria
 
 # The following finds all the leaf folders in the dataset path and stores them in an array
 data_folders=( $(find $dataset_path -type d -mindepth 1 -links 2) )
@@ -20,14 +20,17 @@ do
     save_folder=$save_path/${folder#${dataset_path}}
     # Create the folder if one does not exist already
     mkdir -p $save_folder
-    #ssd_detect /data/stars/share/abdel-internship/caffe/models/VGGNet/VOC0712Plus/SSD_512x512_ft/deploy.prototxt /data/stars/share/abdel-internship/caffe/models/VGGNet/VOC0712Plus/SSD_512x512_ft/VGG_VOC0712Plus_SSD_512x512_ft_iter_160000.caffemodel $source_folder/images_list.txt --confidence_threshold 0.0 --out_file $save_folder/detections-preprocessed.txt
-    /home/aabubakr/ssd-new/build/examples/ssd/ssd_detect /home/aabubakr/ssd-new/models/VGGNet/VOC0712Plus/SSD_512x512_ft/deploy.prototxt /home/aabubakr/ssd-new/models/VGGNet/VOC0712Plus/SSD_512x512_ft/VGG_VOC0712Plus_SSD_512x512_ft_iter_9395.caffemodel $source_folder/images_list.txt --confidence_threshold 0.0 --out_file $save_folder/detections-preprocessed.txt
+    /home/aabubakr/ssd-new/build/examples/ssd/ssd_detect /home/aabubakr/ssd-new/models/VGGNet/VOC0712Plus/SSD_512x512_ft/deploy.prototxt /home/aabubakr/ssd-new/models/VGGNet/VOC0712Plus/VGG_VOC0712Plus_SSD_512x512_ft_iter_1349_inria.caffemodel $source_folder/images_list.txt --confidence_threshold 0.0 --out_file $save_folder/detections-preprocessed.txt
   
     python ../utils/parse-detection-files/image-list-seperate-CalEval.py -DetectionResultsPath $save_folder
+
+   #cp -a $save_folder /home/aabubakr/codes/pedestrian-detection-evaluation/CalEval-3.2.1/data-USA/res/SSD-ft-inria
 
     # Give proper permissions so that we do not have to face any delays due to the permissions issue.
     chmod -R 770 $save_folder 
 done
+
+   cp -a $save_path /home/aabubakr/codes/pedestrian-detection-evaluation/CalEval-3.2.1/data-USA/res/SSD-ft-inria
 
 
 
---
+++ SSD/utils/compute_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#../INRIA/compute_inria.sh
+#../caltech/compute_caltech.sh
+
+../ETHZ/compute_ethz.sh 
+../TUD-Brussels/compute_tud.sh
+../Daimler/compute_daimler.sh
+
